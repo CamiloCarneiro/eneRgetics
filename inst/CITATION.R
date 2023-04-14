779712e8bfd1f51ba6751f0933f1df09dee2ba3b
Camilo Carneiro <53900599+CamiloCarneiro@users.noreply.github.com>
2023-04-14 09:32:25 +0300

    Update CITATION.R

--- inst/CITATION.R
+++ inst/CITATION.R
@@ -1,20 +1,18 @@
 citHeader("To cite eneRgetics in publications use:")
 
-citEntry(entry = "Article",
-         title        = "Maintenance metabolism {eneRgetics}",
-         author       = personList(as.person("CC"),
-                                   as.person("RC"),
-                                   as.person("JAA")),
-         journal      = "MEE",
-         year         = "2019",
+citEntry(entry = "",
+         title        = "eneRgetics",
+         author       = personList(as.person("Camilo Carneiro"),
+                                   as.person("Ricardo Correia"),
+                                   as.person("José A. Alves")),
+         journal      = "",
+         year         = "2020",
+         doi          = "10.5281/zenodo.7827500",
          volume       = "",
          number       = "",
          pages        = "",
          url          = "",
 
          textVersion  =
-           paste("Garrett Grolemund, Hadley Wickham (2011).",
-                 "Dates and Times Made Easy with lubridate.",
-                 "Journal of Statistical Software, 40(3), 1-25.",
-                 "URL http://www.jstatsoft.org/v40/i03/.")
+           paste("")
 )
